26d7454c75f40fc636184a1b38ad87c3edf83362
wolfbiter <wolfbiter@gmail.com>
2017-03-05 11:59:21 -0800

    add default startBeat, audioStartTime, and beatCount to clip getter

--- clips/getters.js
+++ clips/getters.js
@@ -1,10 +1,13 @@
 const { createSelector: Getter } = require('reselect')
-const { mapValues, includes } = require('lodash')
+const { mapValues, includes, get } = require('lodash')
 
+const { isValidNumber, validNumberOrDefault, timeToBeat } = require('../lib/number-utils')
 const { getSamples } = require('../samples/getters')
 
 const getClipsRecords = (state) => state.clips.records
 const getClipsDirty = (state) => state.clips.dirty
+
+const DEFAULT_BEAT_COUNT = 100
 
 const getClips = Getter(
   getClipsRecords,
@@ -14,6 +17,7 @@ const getClips = Getter(
     return mapValues(clips, clip => {
       const sample = samples[clip.sampleId] || {}
 
+      // compute status
       let status = 'unloaded'
       if (sample.isLoading) {
         status = 'loading'
@@ -21,10 +25,28 @@ const getClips = Getter(
         status = 'loaded'
       }
 
+      // compute beatCount
+      let beatCount = clip.beatCount
+      if (!isValidNumber(beatCount)) {
+        if (sample.meta) {
+          const computedBeatCount = timeToBeat(get(sample, 'meta.duration'), get(sample, 'meta.bpm'))
+          beatCount = validNumberOrDefault(computedBeatCount, DEFAULT_BEAT_COUNT)
+        }
+      }
+
+      // compute audioStartTime
+      let audioStartTime = clip.audioStartTime
+      if (!isValidNumber(audioStartTime)) {
+        audioStartTime = validNumberOrDefault(get(sample, 'meta.barGridTime'), 0)
+      }
+
       return {
         ...clip,
         status,
         sample,
+        beatCount,
+        audioStartTime,
+        startBeat: validNumberOrDefault(clip.startBeat, 0),
         isDirty: includes(dirtyClips, clip.id)
       }
     })
